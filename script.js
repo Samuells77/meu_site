798f4a0c713aa8640821f7ea08baf8f2fea866a9
Samuel <38147282+Samuells77@users.noreply.github.com>
2024-11-20 19:17:04 -0300

    20-11-2024

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ ScrollReveal().reveal(".home-content p, about-content", { origin: "right" })
 
 /*==================== Typed js =========================*/
 const typed = new Typed(".multiple-text", {
-  strings: ["Estudante", "Desenhista Detalhista", "Técnico em Informática"],
+  strings: ["Estudante", "Desenhista Projetista", "Técnico em Informática"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
